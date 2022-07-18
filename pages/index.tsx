56235b7f2a21c107d341f38ab62da4bd7d46fa0a
Dan <drblack651@gmail.com>
2022-07-18 18:30:55 -0500

    HN API link added to footer + smol changes on index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,10 +14,10 @@ const Home: NextPage = () => {
 
       <Container>
         <div className='text-center pt-6'>
-          <h2 className="text-amber-600 text-5xl font-bold">Clone of the popular website</h2>
-          <a className='flex flex-col items-center space-y-6 justify-center lg:flex-row lg:space-y-0 lg:space-x-3 group'
+          <h2 className="text-amber-600 text-5xl font-bold">Clone of</h2>
+          <a className='flex flex-col items-center space-y-6 justify-center lg:flex-row lg:space-y-0 lg:space-x-3'
             href="https://news.ycombinator.com" target="_blank" rel="norefferer">
-            <div className='w-32 h-32 order-last lg:order-first mt-6 relative group-hover:brightness-110 duration-200 ease-in-out'>
+            <div className='w-32 h-32 order-last lg:order-first mt-6 relative hover:brightness-110 hover:scale-105 hover:shadow-2xl shadow-lg duration-200 ease-in-out'>
               <Image 
                 src="/Y_Combinator_logo.webp"
                 alt='Y Combinator Logo'
