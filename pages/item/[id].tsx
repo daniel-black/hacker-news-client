82dd0f0c5d5b9a656d922aecb574cd48d5b78401
Dan <drblack651@gmail.com>
2022-07-20 07:24:03 -0500

    render item page server side

--- pages/item/[id].tsx
+++ pages/item/[id].tsx
@@ -1,29 +1,58 @@
-import { NextPage } from "next";
-import { useRouter } from "next/router";
-import axios from 'axios';
-import useSWR from "swr";
+import { GetServerSideProps } from "next";
+import axios from '../../utils/axios';
 import Container from "../../components/structure/container";
 import Item, { ItemProps } from "../../components/item/item";
 
-const fetcher = (url: string) => axios.get(url).then(res => res.data);
+type ItemPageProps = {
+  item: ItemProps,
+  childrenItems: ItemProps[]
+};
 
-const ItemPage: NextPage = () => {
-  const router = useRouter();
-  const { id } = router.query;
-  const route = `https://hacker-news.firebaseio.com/v0/item/${id}.json`;
+const ItemPage = (props: ItemPageProps) => {
+  const { item, childrenItems } = props;
 
-  const { data, error } = useSWR(route, fetcher);
-
-  if (error) return <div>failed to load</div>
-  if (!data) return <div>loading...</div>
-
-  const item: ItemProps = data;
 
   return (
     <Container>
       <Item {...item} />
+      <div className="px-6 space-y-3 mt-3">
+        {childrenItems.map((item, index) => (
+          item.dead || item.deleted ? null : <Item {...item} key={index} />
+        ))}
+      </div>
     </Container>
   );
 }
 
 export default ItemPage;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { params } = context;
+  const itemId = params!.id;
+
+  const { data, status } = await axios.get(`/item/${itemId}.json`);
+
+  if (!data || status !== 200) {
+    return {
+      noFound: true
+    }
+  }
+
+  const item: ItemProps = data;
+  let childrenItems = [] as ItemProps[];
+
+  if (item.kids && item.kids.length > 0) {
+    const requests = item.kids.map(id => axios.get(`/item/${id}.json`));
+    const responses = await Promise.all(requests);
+
+    childrenItems = responses.map(r => r.data);
+  }
+
+  return {
+      props: {
+      item,
+      childrenItems,
+      key: item.id
+    }
+  }
+}
