6834e37d472b83a4085680c3c68448b6a2984d86
Dan <drblack651@gmail.com>
2022-07-22 07:47:55 -0500

    update goal

--- components/buyMeACoffee.tsx
+++ components/buyMeACoffee.tsx
@@ -6,7 +6,7 @@ const BuyMeACoffee = () => {
       <a href='https://www.buymeacoffee.com/blackdan' target='_blank' rel='noreferrer' className='p-2 bg-indigo-100 rounded-3xl hover:bg-indigo-200 duration-150 ease-in-out'>
         <Image src='/buy-me-a-coffee.svg' height='36' width='36' alt='buy me a coffee logo' />
       </a>
-      <p className='font-mono text-sm mt-2 text-center px-4'>If you like my work, pls consider buying me a coffee and helping me reach my goal of making <span className='font-bold'>$1</span> on the internet this year :&nbsp;)</p>
+      <p className='font-mono text-sm mt-2 text-center px-4'>If you like my work, pls consider buying me a coffee and helping me reach my goal of making <span className='font-bold line-through'>$1</span> <span className="font-bold">$100</span> on the internet this year :&nbsp;)</p>
     </>
   )
 }
