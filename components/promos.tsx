e02fb33d2be4f6bb46e1be38200a505e9985ade2
Dan <drblack651@gmail.com>
2022-07-21 21:56:43 -0500

    Modified copy and added emojis

--- components/promos.tsx
+++ components/promos.tsx
@@ -4,7 +4,7 @@ const Promos = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-6 px-2 mb-12 mt-16 text-3xl md:4xl lg:text-5xl font-bold">
 
-      <div className="order-1 flex items-center justify-center text-center mx-6 py-3 md:px-16 leading-relaxed">Explore the profiles of your favorite contributors</div>
+      <div className="order-1 promo-text">🗺️ Explore the profiles of your favorite contributors</div>
 
       <div className='order-2 rotate-2 p-3 m-5 max-w-fit rounded-2xl shadow-lg bg-slate-100 brightness-[102%]  hover:scale-105 hover:-skew-y-1 duration-200 ease-in-out'>
           <Image 
@@ -14,7 +14,7 @@ const Promos = () => {
           />
         </div>
       
-      <div className="order-3 md:order-4 flex items-center justify-center text-center mx-6 py-3 md:px-16 leading-relaxed">Stay up to date with the latest in tech</div>
+      <div className="order-3 md:order-4 promo-text">👩‍💻 Stay up to date with the latest in tech</div>
 
       <div className='order-4 md:order-3 -rotate-2 p-3 max-w-fit hover:scale-105 hover:skew-y-1 duration-200 ease-in-out'>
         <Image 
@@ -24,7 +24,7 @@ const Promos = () => {
         />
       </div>
 
-      <div className="order-5 flex items-center justify-center text-center mx-6 py-3 md:px-16 leading-relaxed">See what types of things your peers are posting about</div>
+      <div className="order-5 promo-text">💬 See the interesting things surfaced by interesting people</div>
 
       <div className='order-6 rotate-2 p-3 max-w-fit rounded-2xl shadow-lg bg-slate-100 brightness-[102%] hover:scale-105 hover:-skew-y-1 duration-200 ease-in-out'>
         <Image 
